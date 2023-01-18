7d93b9161ad8dc2ab80d917df442c5373d65a85c
S. Roland Marshall <samuel.r.marshall@gmail.com>
2023-01-17 21:26:43 -0500

    create nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :draft_picks
   resources :teams
-  resources :fantasy_teams
   # route for stimulus autocomplete
   get '/players/search', to: 'players#search', as: 'search_players'
   resources :players
@@ -9,7 +8,11 @@ Rails.application.routes.draw do
   # create a "start_draft_path" route that turns active to true
   put '/drafts/:id/start_draft', to: 'drafts#start_draft', as: 'start_draft'
   post '/drafts/:id/reset', to: 'drafts#reset', as: 'reset_draft'
-  resources :fantasy_leagues
+  resources :fantasy_leagues do
+    # nested resources for fantasy_leagues/teams, which is an alias for fantasy_teams
+    resources :teams, controller: 'fantasy_teams'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
