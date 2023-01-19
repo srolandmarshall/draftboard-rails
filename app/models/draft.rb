951cff575183e27aa2664a0512fbc5e59371c129
S. Roland Marshall <samuel.r.marshall@gmail.com>
2023-01-18 21:33:41 -0500

    improve draft model

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -14,40 +14,33 @@ class Draft < ApplicationRecord
     update(current_pick: 1, order: generate_order)
   end
 
+  # generates an array of fantasy team IDs that represents the draft order
   def generate_order
-    # generate a hash of a snake draft order from the fantasy teams belonging to the league
-
-    # randomly select the order of the first round
-    order = fantasy_teams.shuffle
-    order_hash = {}
-    # each team will have a number of picks equal to the roster_size
-    # the order is a function of roster_size each draft has
-    num_of_picks = roster_size * fantasy_teams.count
-    pick = 1
-    while pick <= num_of_picks
-      order.each do |team|
-        order_hash[pick] = team.id
-        pick += 1
-      end
-      # each round will be a mirror of the previous round
-      order.reverse!
+    order = fantasy_teams.shuffle.map(&:id)
+    order_array = []
+    roster_size.times do |i|
+      order_array += i.odd? ? order : order.reverse
     end
-    order_hash
+    order_array
   end
 
   def order_with_teams
-    # for each pair in the order hash, find the team and return a hash of the pick and team
-    order.map do |pick, team_id|
-      { pick:, team: FantasyTeam.find_by(id: team_id) }
+    # for each pair in the order array, find the team and return a hash of the pick and team
+    order.map.with_index do |team_id, index|
+      { pick: index + 1, team: FantasyTeam.find_by(id: team_id) }
     end
   end
 
+  def current_pick_index
+    current_pick.to_i - 1
+  end
+
   def current_team
-    fantasy_teams.find_by(id: order[current_pick.to_s])
+    fantasy_teams.find_by(id: order[current_pick_index])
   end
 
   def team_by_pick(pick)
-    fantasy_teams.find_by(id: order[pick.to_s])
+    fantasy_teams.find_by(id: order[pick.to_i - 1])
   end
 
   def increment_pick!
