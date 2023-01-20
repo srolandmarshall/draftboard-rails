9f74371c2b30603b77a526197d3a3ea4ff3a147a
S. Roland Marshall <samuel.r.marshall@gmail.com>
2023-01-20 17:03:59 -0500

    allow get reset

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   resources :drafts
   # create a "start_draft_path" route that turns active to true
   put '/drafts/:id/start_draft', to: 'drafts#start_draft', as: 'start_draft'
-  post '/drafts/:id/reset', to: 'drafts#reset', as: 'reset_draft'
+  # allow get or post to reset draft
+  match '/drafts/:id/reset', to: 'drafts#reset', as: 'reset_draft', via: %i[get post]
   resources :fantasy_teams
   resources :fantasy_leagues do
     resources :teams, controller: 'fantasy_teams'
