cd93738d88c8ecf253028219981a7561d42d0577
S. Roland Marshall <samuel.r.marshall@gmail.com>
2023-01-19 21:09:55 -0500

    added round to a DraftPick

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_065103) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_020910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_065103) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "pick_number"
+    t.integer "round"
     t.index ["draft_id"], name: "index_draft_picks_on_draft_id"
     t.index ["fantasy_team_id"], name: "index_draft_picks_on_fantasy_team_id"
     t.index ["player_id"], name: "index_draft_picks_on_player_id"
