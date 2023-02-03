0dd436436251883501365b5813b7da5cab928215
S. Roland Marshall <samuel.r.marshall@gmail.com>
2023-02-02 21:51:39 -0500

    remove firestore controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,16 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import DraftController from "./draft_controller"
-application.register("draft", DraftController)
+import DraftController from "./draft_controller";
+application.register("draft", DraftController);
 
-import DraftpickController from "./draftpick_controller"
-application.register("draftpick", DraftpickController)
+import DraftpickController from "./draftpick_controller";
+application.register("draftpick", DraftpickController);
 
-import FirestoreController from "./firestore_controller"
-application.register("firestore", FirestoreController)
-
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
